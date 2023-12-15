c3ce8a8d7e2ba3a63816217662a5f0657c9bfbec
Godwin Bruce <godwin.bruce@gmail.com>
2023-12-14 19:14:39 -0800

    palling

---
+++ stack_pall.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * mo_pall - prints stack pall
+ * @master: head input
+ * @reader: read line
+ * Return: 0 (success)
+*/
+void mo_pall(stack_t **master, unsigned int reader)
+{
+	stack_t *m;
+	(void)readeer;
+
+	m = *master;
+	if (m == NULL)
+		return;
+	while (m)
+	{
+		printf("%d\n", m->n);
+		m = m->next;
+	}
+}
