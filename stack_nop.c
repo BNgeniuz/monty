defb3ed4a9c6f7dd10074a2206a379fca27f4f42
Godwin Bruce <godwin.bruce@gmail.com>
2023-12-14 19:02:26 -0800

    nopping

---
+++ stack_nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * mo_nop- nop
+ * @master: head input
+ * @readter: line read
+ * Return: no return
+ */
+void mo_nop(stack_t **master, unsigned int reader)
+{
+	(void) reader;
+	(void) master;
+}
