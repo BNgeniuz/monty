7abe6aa1f62332917e0c3ddf6e5e0fa2011a8c87
Godwin Bruce <godwin.bruce@gmail.com>
2023-12-14 20:10:48 -0800

    stacks lifi

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * mo_stack - prints the top
+ * @master: head input
+ * @reader: line read
+ * Return: 0 (success)
+*/
+void mo_stack(stack_t **master, unsigned int reader)
+{
+	(void)master;
+	(void)readeer;
+	bus.lifi = 0;
+}
